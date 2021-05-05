caeb9fa838f11d86a81d0177e8c973ac50576eef
jiayuzheng01 <jiayuzheng01@gmail.com>
2021-05-05 14:42:16 -0400

    Saving changes before mirgrate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,37 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index(Request $request){
       return $request->user();
     }
-  
+
     public function register(Request $request){
       $validator = Validator::make($request->all(), [
         'email' => 'nullable|email|max:64',
         'password' => 'required|string',
       ]);
-      
+
       if ($validator->fails()){
         return response(['message' => 'Validation errors', 'errors' -> $validator->errors(), 'status' => false], 422);
       }
-      
+
       $input = $request->all();
       $input['password'] = Hash::make($input['password']);
       $user = User::create($input);
-      
+
       // User auth access token is generated below
       $data['token'] = $user->createToken('WePlan')->accessToken;
       $data['user_data'] = $user;
-      
+
       return response(['data' => $data, 'message' => 'Account created successfully!', 'status' => true]);
     }
-  
+
     public function logout(){
       if (Auth::check()) {
         Auth::user()->token()->revoke();
