6dc047aed3491c3b5447db41e52c9ada50003d69
jiayuzheng01 <jiayuzheng01@gmail.com>
2021-05-05 15:42:19 -0400

    Made create function in GroupController

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Group;
+use App\Models\User;
 
 class GroupController extends Controller
 {
-    //
+    // public function index () {
+
+    // }
+
+    public function create(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if ($user) {
+            $groupName = Group::find($request->name);
+
+            if ($groupName) {
+                return response()->json(['error' => 'There is already a group with this name! Please try another name.'], 422);
+            } else {
+                $input = $request->all();
+                $group = Group::create($input);
+
+                return response()->json(['data' => $group, 'message' => 'Group created successfully!'], 200);
+            }
+
+
+        } else {
+            return response()->json(['error' => 'You do not have access to make a group. Create an account first and then try again!'], 422);
+        }
+    }
+
+    // public function update () {
+
+    // }
+
+    // public function delete () {
+
+    // }
+
 }
